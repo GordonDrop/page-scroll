d19aaa501c0b68b068da26f07d8988b0dd8df10d
ksimanov <ksimanov@ronasit.com>
2016-11-27 16:45:59 +0600

    - Add init logic

--- src/page-scroll.js
+++ src/page-scroll.js
@@ -6,25 +6,58 @@
 
   // The actual plugin constructor
   function Plugin(el, options) {
-    this.$el = el;
+    this.$el = $(el);
+    this.$body = $('body');
+    this.$doc = $(document);
+    this.$win = $(window);
+    this.$pages = this.$el.find('.page');
 
-    this.settings = $.extend({}, defaults, options);
-    this._defaults = defaults;
-    this._name = pluginName;
+    // TODO: add settings uncomment
+    // this.settings = $.extend({}, defaults, options);
+
     this.init();
+    this.bindEvents();
   }
 
   $.extend(Plugin.prototype, {
     init: function () {
-      console.log('init');
+      var plugin = this;
+
+      this.activePage = 0;
+      this.vpHeight = this.$win.height();
+
+      this.$body.css({
+        'overflow': 'hidden',
+        'height': '100%'
+      });
+
+      this.$el.addClass('page-scroll-section');
+
+      this.$pages.each(function (i, page) {
+        $(page).attr('data-id', i);
+
+        if ($(page).height() > plugin.vpHeight) {
+          $(page).addClass('scrollable');
+        }
+      });
+
+      this.$pages
+        .wrap('<div class="page-scroll-section">');
+
+      this.$sections = $('.page-scroll-section');
+      this.$sections.css({ 'height': this.vpHeight + 'px' });
+    },
+
+    bindEvents: function () {
+
     }
   });
 
   $.fn[pluginName] = function (options) {
     return this.each(function () {
-      if (!$.data(this, "plugin_" + pluginName)) return;
+      if ($.data(this, pluginName)) return;
 
-      $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+      $.data(this, pluginName, new Plugin(this, options));
     });
   };
 
